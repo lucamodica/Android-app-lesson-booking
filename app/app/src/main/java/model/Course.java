b1f62c1fea43792c079be84ec729ac53f84b7f5c
Salvo Ruvolo <salvatore.ruvolo@edu.unito.it>
2021-11-30 10:21:09 +0100

    DAO DA IMPLEMENTARE

---
+++ app/app/src/main/java/model/Course.java
@@ -0,0 +1,48 @@
+package model;
+
+import java.io.Serializable;
+
+public class Course implements Serializable,sGson {
+private String course_title;
+private String desc;
+
+    public Course() {
+        course_title="";
+        desc="";
+    }
+
+    public Course(String course_title, String desc) {
+        this.course_title = course_title;
+        this.desc = desc;
+    }
+
+    public String getCourse_title() {
+        return course_title;
+    }
+
+    public void setCourse_title(String course_title) {
+        this.course_title = course_title;
+    }
+
+    public String getDesc() {
+        return desc;
+    }
+
+
+    @Override
+    public String toString() {
+        return "Course{" +
+                "course_title='" + course_title + '\'' +
+                ", desc='" + desc + '\'' +
+                '}';
+    }
+
+    public void setDesc(String desc) {
+        this.desc = desc;
+    }
+
+    @Override
+    public Gson sendJson() {
+
+    }
+}
