8ab8e8e19fa15d2e9f2fcac93b1835298a271b1c
Luca Modica <luca.modica@edu.unito.it>
2021-12-07 17:56:13 +0100

    Folder reorganization

---
+++ app/src/main/java/com/example/myapplication/view/activity/MainActivity.java
@@ -0,0 +1,51 @@
+package com.example.myapplication.view.activity;
+
+import android.content.Context;
+import android.content.Intent;
+import android.os.Bundle;
+
+import com.example.myapplication.R;
+import com.google.android.material.bottomnavigation.BottomNavigationView;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.navigation.NavController;
+import androidx.navigation.Navigation;
+import androidx.navigation.ui.AppBarConfiguration;
+import androidx.navigation.ui.NavigationUI;
+
+import com.example.myapplication.databinding.ActivityMainBinding;
+
+public class MainActivity extends AppCompatActivity {
+
+    private ActivityMainBinding binding;
+    private static Context context;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        binding = ActivityMainBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
+
+        BottomNavigationView navView = findViewById(R.id.nav_view);
+        // Passing each menu ID as a set of Ids because each
+        // menu should be considered as top level destinations.
+        AppBarConfiguration appBarConfiguration = new AppBarConfiguration.Builder(
+                R.id.navigation_home, R.id.navigation_booking, R.id.navigation_account)
+                .build();
+        NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment_activity_main);
+        NavigationUI.setupActionBarWithNavController(this, navController, appBarConfiguration);
+        NavigationUI.setupWithNavController(binding.navView, navController);
+
+
+        //login part
+        Intent inte = new Intent(this, LoginActivity.class);
+
+        // aggiungo stringa in piu (es risultato)
+        setResult(LoginActivity.RESULT_OK, inte);
+        startActivity(inte);
+
+    }
+
+
+    }
+
